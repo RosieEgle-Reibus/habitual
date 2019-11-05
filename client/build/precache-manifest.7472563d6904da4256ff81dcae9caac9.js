d92eed85fb31aa64468e85eb8a612cf0eb55de24
Rose Egle <rosiemegle89@gmail.com>
2019-11-05 14:38:02 -0500

    initial commit

---
+++ client/build/precache-manifest.7472563d6904da4256ff81dcae9caac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f4aff68f1db34743215f77a8afc8a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea7f054e88fc40b224d8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "ea7f054e88fc40b224d8",
+    "url": "/static/js/main.7bea4b7b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
