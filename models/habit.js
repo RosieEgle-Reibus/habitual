3fb3987d4e89e8d9972fa0e74f10469111635fa1
Rose Egle <rosiemegle89@gmail.com>
2019-11-05 15:08:50 -0500

    made models for habits and updated server.js/connections.js

---
+++ models/habit.js
@@ -0,0 +1,58 @@
+
+const mongoose = require('./connection.js')
+
+const HabitSchema = new mongoose.Schema({
+  habit: String,
+  expectedTimesPerDay: Number,
+  totalTimesCompleted: Number,
+  difficulty: Number,
+  userId: Number,
+
+})
+
+const HabitCollection = mongoose.model('Habit', HabitSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+
+ //getAll
+ const getAllHabits = () => {
+   return HabitCollection.find()
+ }
+
+ //getOne
+ const getOneHabit = (habitId) => {
+   return HabitCollection.findById({_id: habitId})
+ }
+
+ //creature
+ const createHabit = (habitData) => {
+  return HabitCollection.create(habitData)
+ }
+
+ //update
+ const updateHabit = (habitId, habitData) => {
+  return HabitCollection.update({_id: habitId}, habitData)
+ }
+
+ //delete
+ const deleteHabit = (habitId) => {
+   return HabitCollection.deleteOne({_id: habitId})
+ }
+
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getAllHabits,
+  getOneHabit,
+  createHabit,
+  updateHabit,
+  deleteHabit
+}
